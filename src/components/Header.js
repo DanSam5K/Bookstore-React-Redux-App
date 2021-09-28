7594733894ef1a2ec862ac0d308bdeada22915c8
Daniel Samuel <dansam5000@gmail.com>
2021-09-28 11:51:10 +0100

    Add Header component element

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>BOOKSTORE APP</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link className="link" to="/Books">
+              Books
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/Categories">
+              Categories
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
