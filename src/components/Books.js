61d8f4bf800821e45d0a8241a76a4df1ed34dba1
Daniel Samuel <dansam5000@gmail.com>
2021-09-29 12:27:10 +0100

    Move store provider to top level component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import AddNewBook from './AddNewBook';
 import ShowBooks from './ShowBooks';
-import store from '../redux/configureStore';
 
 const Books = () => (
-  <Provider store={store}>
+  <>
     <ShowBooks />
     <AddNewBook id="categories" name="categories" selected />
-  </Provider>
+  </>
 );
 
 export default Books;
