6cced748aecdc9e5dfc04e8e7d1e17f37efc0c41
Daniel Samuel <dansam5000@gmail.com>
2021-09-29 04:23:17 +0100

    Add actioncreatore by importing miss dependencies

--- src/action/actionCreator.js
+++ src/action/actionCreator.js
@@ -1,3 +1,6 @@
+import { v4 as uuidv4 } from "uuid";
+import * as actions from "./bookAction";
+
 export const addBook = ({ title, author, genre }) => ({
   type: actions.ADDED_BOOK,
   id: uuidv4(),
