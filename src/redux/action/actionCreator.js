43cd70a937c8e32d1ec4a82713af79a76bb15b63
Daniel Samuel <dansam5000@gmail.com>
2021-09-30 23:33:29 +0100

    Redesign action creators with axios call

--- src/redux/action/actionCreator.js
+++ src/redux/action/actionCreator.js
@@ -1,16 +1,48 @@
+import axios from 'axios';
 import * as actions from './bookAction';
+import baseURL from './api';
 
-export const addBook = (book) => ({
-  type: actions.ADD_BOOK,
-  book,
-});
+const urlAPI = `${baseURL}/books`;
+export const addBook = (book) => async (dispatch) => {
+  const res = await axios.post(urlAPI, {
+    item_id: book.item_id,
+    title: book.title,
+    category: book.category,
+  });
+  const created = await res.data;
+  if (created === 'Created') {
+    dispatch({
+      type: actions.ADD_BOOK,
+      book,
+    });
+  }
+};
 
-export const removeBook = (book) => ({
-  type: actions.REMOVE_BOOK,
-  book,
-});
+export const removeBook = (bookId) => async (dispatch) => {
+  const res = await axios.delete(`${urlAPI}/${bookId}`);
+  const deleted = await res.data;
+  if (deleted) {
+    dispatch({
+      type: actions.REMOVE_BOOK,
+      bookId,
+    });
+  }
+};
 
-export const loadBook = (book) => ({
-  type: actions.RETURN_BOOK,
-  book,
-});
+export const loadBook = () => async (dispatch) => {
+  const res = await axios.get(urlAPI);
+  const data = await res.data;
+  if (data) {
+    const objectArray = Object.entries(data);
+    const booksArray = [];
+    objectArray.forEach(([key, value]) => {
+      const item = { ...value, item_id: key };
+      const itemObj = Object.values(item);
+      booksArray.push({ ...itemObj[0], item_id: itemObj[1] });
+    });
+    dispatch({
+      type: actions.RETURN_BOOK,
+      booksArray,
+    });
+  }
+};
