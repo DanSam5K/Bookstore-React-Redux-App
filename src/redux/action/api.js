331d6404afcf3f76d0399468374e80c7945a3ec1
Daniel Samuel <dansam5000@gmail.com>
2021-09-30 23:31:12 +0100

    Relocate api file to action folder

---
+++ src/redux/action/api.js
@@ -0,0 +1,4 @@
+const baseURL =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/CBvlUI2K2HpcwfRuxbFJ';
+
+export default baseURL;
--- src/redux/books/api.js
+++
@@ -1,3 +0,0 @@
-const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/CBvlUI2K2HpcwfRuxbFJ';
-
-export default baseURL;
