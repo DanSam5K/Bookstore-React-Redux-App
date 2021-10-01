d5d2fee1d0a124f6478be69992376ecc5667a93c
Daniel Samuel <dansam5000@gmail.com>
2021-10-01 11:19:55 +0100

    Add styles to App and Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../assets/css/navbar.css';
+import logo from '../assets/images/logo.svg';
 
 const Navbar = () => (
   <div>
-    <h1>BOOKSTORE APP</h1>
-    <nav>
-      <ul>
-        <li>
-          <Link className="link" to="/Books">
-            Books
+    <header className="m-b bg-white round-top">
+      <div className="center max-width-90 flex-row">
+        <h1 className="app-title">BOOKSTORE CMS APP</h1>
+        <div className="cat-title">
+          <Link className="text-grey" to="/BookList">
+            Book
           </Link>
-        </li>
-        <li>
-          <Link className="link" to="/Categories">
+        </div>
+        <div className="cat-title">
+          <Link className="text-grey" to="/Categories">
             Categories
           </Link>
-        </li>
-      </ul>
-    </nav>
+        </div>
+        <img className="react-logo" src={logo} alt="logo" />
+      </div>
+    </header>
   </div>
 );
 
